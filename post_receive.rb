9e95b8b9ea5261c77fdee0d0ffd420c8255abc79
Matti Paksula <matti.paksula@iki.fi>
2016-04-24 21:56:45 +0300

    Use kontena.REPOSITORY_NAME.yml to build if available. (#2)

--- post_receive.rb
+++ post_receive.rb
@@ -8,6 +8,8 @@ if (branch =~ /master$/) == nil
     exit
 end
 
+repository_name = File.basename(File.expand_path("."))
+
 deploy_to_dir = File.expand_path('./.deploy')
 unless Dir.exist?(deploy_to_dir)
   Dir.mkdir(deploy_to_dir)
@@ -16,7 +18,11 @@ end
 
 Dir.chdir(deploy_to_dir)
 if File.exist?('./kontena-build.yml')
+  puts "Using file kontena-build.yml to build."
   exec 'kontena', 'app', 'build', '-f', 'kontena-build.yml'
+elsif File.exist?("./kontena.#{repository_name}.yml")
+  puts "Using file kontena.#{repository_name}.yml to build."
+  exec 'kontena', 'app', 'build', '-f', "kontena.#{repository_name}.yml"
 else
   exec 'kontena', 'app', 'build'
 end
